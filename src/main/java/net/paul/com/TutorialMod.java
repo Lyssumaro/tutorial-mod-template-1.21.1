d1efd41c05830d15ed4bdcb52f40697eeba1882e
Paul <paulhoerdt07@gmail.com>
2025-10-15 18:18:30 +0200

    Update

---
+++ src/main/java/net/paul/com/TutorialMod.java
@@ -0,0 +1,23 @@
+package net.paul.com;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.paul.com.Items.ModItemGroups;
+import net.paul.com.Items.ModItems;
+import net.paul.com.block.ModBlocks;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+        // Very important comment
+public class TutorialMod implements ModInitializer {
+	public static final String MOD_ID = "tutorialmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+        ModItemGroups.registerItemGroups();
+
+        ModItems.registerModItems();
+        ModBlocks.registerModBlocks();
+    }
+}
