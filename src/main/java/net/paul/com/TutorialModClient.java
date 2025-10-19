b2693df2be2bc56d765ab170ad2d0ce51a0fb74c
Paul <paulhoerdt07@gmail.com>
2025-10-19 19:33:35 +0200

    Update

---
+++ src/main/java/net/paul/com/TutorialModClient.java
@@ -0,0 +1,14 @@
+package net.paul.com;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
+import net.paul.com.block.ModBlocks;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.PINK_GARNET_DOOR, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.PINK_GARNET_TRAPDOOR, RenderLayer.getCutout());
+    }
+}
